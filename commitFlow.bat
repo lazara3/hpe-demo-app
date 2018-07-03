ac9f6a93e49c4101bd2011a205ce15bf28aaed45
Ayellet <layellet@gmail.com>
2018-07-03 16:53:46 +0300

    Update commitFlow.bat

--- commitFlow.bat
+++ commitFlow.bat
@@ -1,1 +1,17 @@
-this is my awesome text file 
+if exist "MyFile.txt" (
+	
+	git push origin gocd
+	
+	git rm MyFile.txt
+	git commit -m "remove MyFile.txt"
+	git push origin gocd
+
+	echo Exist and delete it
+	
+) else (
+	echo this is my awesome text file > MyFile.txt
+	git add .
+	git commit -m "Add MyFile.txt to branch GoCD"
+
+	echo create and commit the file
+)
